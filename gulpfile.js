22e15005c8bf90256ec57ec2e2eb7ef50d59d88a
Pascal Breton-Faguy <pascal.breton-faguy@libeo.com>
2016-03-14 14:34:38 -0400

    Changé gulp-minify pour gulp-uglify

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,21 @@
 var gulp = require('gulp');
+var rename = require('gulp-rename');
 var clean = require('gulp-clean');
-var minify = require('gulp-minify');
+var uglify = require('gulp-uglify');
 
 gulp.task('clean', function() {
     return gulp.src('dist/**/*', { read: false })
         .pipe(clean());
 });
 
-gulp.task('minify', function() {
-    return gulp.src('src/*.js')
-        .pipe(minify())
-        .pipe(gulp.dest('dist'))
+gulp.task('uglify', function() {
+	return gulp.src('src/*.js')
+	// This will output the non-minified version
+	.pipe(gulp.dest('dist'))
+	// This will minify and rename to foo.min.js
+	.pipe(uglify())
+	.pipe(rename({ extname: '.min.js' }))
+	.pipe(gulp.dest('dist'));
 });
 
-gulp.task('default', ['clean', 'minify']);
+gulp.task('default', ['clean', 'uglify']);
